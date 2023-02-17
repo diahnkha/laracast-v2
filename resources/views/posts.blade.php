323f028a34380ac6e5d002273e9f481326477a58
Diah Nur Khasanah <diahnurkhasanah@Diahs-MacBook-Pro.local>
2023-02-17 15:48:33 +0700

    add css and grid component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,19 +2,11 @@
 
         @include('_posts-header')
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card :post="$posts[0]"/>
-
-            <div class="lg:grid lg:grid-cols-2">
-                <x-post-card />
-                <x-post-card />
-            </div>
-
-            <div class="lg:grid lg:grid-cols-3">
-
-                <x-post-card />
-                <x-post-card />
-                <x-post-card />
-            </div>
+            @if ($posts->count())
+                <x-posts-grid :posts="$posts"/>
+            @else
+                <p class="text-center">No Post Yet. Check Back Later</p>
+            @endif
         </main>
 
 </x-layout>
