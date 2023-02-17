ffdf3c95e732af60258bbbce0430a1c1953a29a1
Diah Nur Khasanah <diahnurkhasanah@Diahs-MacBook-Pro.local>
2023-02-17 21:21:44 +0700

    add half dropdown extract

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,11 @@
+@props(['trigger'])
+<div x-data="{show: false}" @click.away="show=false">
+{{-- Trigger --}}
+<div @click="show = ! show">
+    {{ $trigger }}
+</div>
+
+{{-- Links --}}
+<div x-show="show" class="py-2 absolute w-full mt-2 bg-gray-100 rounded-xl z-50" style="display:none"> 
+    {{ $slot }}
+</div>
