280051ee3febdbdec0c2de9c0f2d8c9a3dda417e
Diah Nur Khasanah <diahnurkhasanah@Diahs-MacBook-Pro.local>
2023-02-18 11:22:39 +0700

    add quick tweaks and clean up

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,6 +6,6 @@
 </div>
 
 {{-- Links --}}
-<div x-show="show" class="py-2 absolute w-full mt-2 bg-gray-100 rounded-xl z-50" style="display:none"> 
+<div x-show="show" class="py-2 absolute w-full mt-2 bg-gray-100 rounded-xl z-50 overflow-auto max-h-52" style="display:none"> 
     {{ $slot }}
 </div>
