d14894402a9117c2f9e7b1803b7949dc9f8bef4e
Diah Nur Khasanah <diahnurkhasanah@Diahs-MacBook-Pro.local>
2023-02-19 01:31:32 +0700

    add failed validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,8 +16,9 @@ class RegisterController extends Controller
         // var_dump(request()->all());
         $attributes = request()->validate([
             'name' => 'required|max:255',
-            'username' => 'required|max:255|min:3',
-            'email' => 'required|email|max:255',
+            // 'username' => ['required', 'min:3', 'max:225', Rule::unique('users', 'username')],
+            'username' => 'required|max:255|min:3|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|min:7|max:255'
         ]);
 
