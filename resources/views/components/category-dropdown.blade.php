7d637f21196cae1615609eb9eedc618fd0511ec9
Diah Nur Khasanah <diahnurkhasanah@Diahs-MacBook-Pro.local>
2023-02-18 21:40:38 +0700

    merge category and search

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -11,11 +11,15 @@
 
     <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 
+    @php
+        ['name' => 'john']
+    @endphp
+
 
     @foreach($categories as $category)
 
     <x-dropdown-item 
-    href="/?category={{ $category->slug }}"
+    href="/?category={{ $category->slug }} && {{http_build_query(request()->except('category'))}}"
     :active='request()->is("/categories.{$category->slug}")'
     >{{ ucwords($category->name) }}</x-dropdown-item>
 
